e27ffea4347ff54c92e0612fdb492a996399a428
Basit ali <basitalyshah51214@gmail.com>
2025-12-05 22:40:03 -0800

    Add AppColors class for managing color constants

---
+++ lib/controllers/app_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/widgets.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFFFFFFFF);
+  static const Color primaryVariant = Color(0xFF3700B3);
+  static const Color secondary = Color(0xFFD01005);
+  static const Color secondaryVariant = Color(0xFF018786);
+  static const Color background = Color(0xff000000);
+
+  static const Color surface = Color(0xFFFFFFFF);
+  static const Color onPrimary = Color(0xFFFFFFFF);
+  static const Color onSecondary = Color(0xFF000000);
+  static const Color onBackground = Color(0xFF000000);
+  static const Color onSurface = Color(0xFF000000);
+  static const Color onError = Color(0xFFFFFFFF);
+}
