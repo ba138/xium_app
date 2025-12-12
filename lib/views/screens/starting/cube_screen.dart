46d22ff855fc9b8af75da210a7ea8301f004f6c1
Basit ali <basitalyshah51214@gmail.com>
2025-12-12 12:27:34 -0800

    add cube animation to the cube screen

--- lib/views/screens/starting/cube_screen.dart
+++ lib/views/screens/starting/cube_screen.dart
@@ -1,43 +1,88 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:xium_app/constants/app_colors.dart';
-import 'package:xium_app/generated/assets.dart';
 import 'package:xium_app/views/screens/home/home_screen.dart';
-import 'package:xium_app/views/widgets/common_image_view.dart';
 import 'package:xium_app/views/widgets/my_Text.dart';
 
-class CubeScreen extends StatelessWidget {
+class CubeScreen extends StatefulWidget {
   const CubeScreen({super.key});
+
+  @override
+  State<CubeScreen> createState() => _CubeScreenState();
+}
+
+class _CubeScreenState extends State<CubeScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> rotation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3), // 🔥 rotate for EXACT 3 SECONDS
+    );
+
+    rotation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOutCubic,
+    );
+
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        Get.offAll(() => const HomeScreen());
+      }
+    });
+  }
+
+  void rotateAndGo() {
+    _controller.forward(from: 0);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xff050B18),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: GestureDetector(
-            onTap: () {
-              Get.offAll(() => HomeScreen());
-            },
-            child: Column(
-              children: [
-                SizedBox(height: MediaQuery.of(context).size.height * 0.05),
-                CommonImageView(imagePath: Assets.cube),
-                Spacer(),
-                Icon(
-                  Icons.keyboard_arrow_up_sharp,
-                  size: 60,
-                  color: AppColors.onPrimary,
+        child: GestureDetector(
+          onTap: rotateAndGo,
+          child: Column(
+            children: [
+              const SizedBox(height: 30),
+
+              Expanded(
+                child: Center(
+                  child: AnimatedBuilder(
+                    animation: rotation,
+                    builder: (context, _) {
+                      return Transform.rotate(
+                        angle: rotation.value * pi,
+                        child: Image.asset(
+                          "assets/images/cube.png",
+                          width: 260,
+                          height: 260,
+                        ),
+                      );
+                    },
+                  ),
                 ),
-                Icon(
-                  Icons.keyboard_arrow_up_sharp,
-                  size: 40,
-                  color: AppColors.onPrimary,
-                ),
-                MyText(text: "Tap to enter your XIUM space"),
-                const SizedBox(height: 80),
-              ],
-            ),
+              ),
+
+              Icon(Icons.keyboard_arrow_up, size: 50, color: Colors.white),
+              Icon(Icons.keyboard_arrow_up, size: 35, color: Colors.white),
+
+              const SizedBox(height: 8),
+
+              MyText(
+                text: "Tap to enter your XIUM space",
+                size: 16,
+                color: Colors.white,
+              ),
+
+              const SizedBox(height: 80),
+            ],
           ),
         ),
       ),
