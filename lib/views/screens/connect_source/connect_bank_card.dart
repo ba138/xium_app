cbb640f04a526dd4562f88dfb237d5ffdce09870
ba138 <basitalyshah51214@gmail.com>
2026-01-11 11:53:06 +0500

    refactor: remove unused screens and related code for cleaner structure

--- lib/views/screens/connect_source/connect_bank_card.dart
+++ lib/views/screens/connect_source/connect_bank_card.dart
@@ -3,11 +3,9 @@ import 'package:get/get.dart';
 import 'package:xium_app/constants/app_colors.dart';
 import 'package:xium_app/controller/plaid_controller.dart';
 import 'package:xium_app/generated/assets.dart';
-import 'package:xium_app/views/screens/connect_source/redirecting_screen.dart';
 import 'package:xium_app/views/widgets/common_image_view.dart';
 import 'package:xium_app/views/widgets/my_button.dart';
 import 'package:xium_app/views/widgets/my_text.dart';
-import 'package:xium_app/views/widgets/my_text_field.dart';
 
 class ConnectBankCard extends StatefulWidget {
   const ConnectBankCard({super.key});
@@ -90,89 +88,6 @@ class _ConnectBankCardState extends State<ConnectBankCard> {
           ),
         ),
       ),
-    );
-  }
-
-  void _openBankSelectionSheet() {
-    Get.bottomSheet(
-      Container(
-        height: Get.height * 0.75,
-        padding: const EdgeInsets.all(20),
-        decoration: BoxDecoration(
-          color: Color(0xff0C1A44), // GREEN COLOR
-          borderRadius: const BorderRadius.vertical(top: Radius.circular(25)),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: Container(
-                height: 4,
-                width: 70,
-                decoration: BoxDecoration(
-                  color: AppColors.grayColor,
-                  borderRadius: BorderRadius.circular(4),
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            Center(
-              child: MyText(
-                text: "Select Bank",
-                size: 20,
-                weight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 20),
-
-            MyTextField(
-              hint: "Search bank",
-              prefix: Icon(Icons.search, color: AppColors.primary),
-              radius: 12,
-            ),
-
-            const SizedBox(height: 20),
-
-            Expanded(
-              child: ListView(
-                children: [
-                  _bankTile("Revolut", Assets.revolut),
-                  const SizedBox(height: 10),
-                  _bankTile("N26", Assets.n26),
-                  const SizedBox(height: 10),
-
-                  _bankTile("Monzo", Assets.monzo),
-                  const SizedBox(height: 10),
-
-                  _bankTile("Barclays", Assets.barclays),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-      isScrollControlled: true,
-    );
-  }
-
-  Widget _bankTile(String name, String logo) {
-    return Column(
-      children: [
-        GestureDetector(
-          onTap: () {
-            Get.to(() => RedirectingScreen());
-          },
-          child: Row(
-            children: [
-              CommonImageView(imagePath: logo, height: 40),
-              const SizedBox(width: 12),
-              MyText(text: name, size: 18, weight: FontWeight.bold),
-            ],
-          ),
-        ),
-        const SizedBox(height: 10),
-        Divider(color: AppColors.grayColor, thickness: 0.5),
-      ],
     );
   }
 }
